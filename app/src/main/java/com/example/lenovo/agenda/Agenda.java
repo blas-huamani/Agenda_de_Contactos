45e18e0671b01e810ac06abcebc98e74d8485623
blas-huamani <nokialumiablas@hotmail.com>
2016-11-14 21:08:35 -0500

    Update Agenda.java

--- app/src/main/java/com/example/lenovo/agenda/Agenda.java
+++ app/src/main/java/com/example/lenovo/agenda/Agenda.java
@@ -1,6 +1,5 @@
 package com.example.lenovo.agenda;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
@@ -20,45 +19,56 @@ import java.util.ArrayList;
 
 
 
----------------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------------------------------------------
 public class Agenda extends Activity {
+   
+    
     private ArrayList<String> resultados = new ArrayList<String>();
     private ListView lv;
     private EditText name;
 
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda);
 
         name = (EditText) findViewById(R.id.nombre);
         lv = (ListView) findViewById(R.id.listView);
 
+        
         abrirDBAdapter();
+        
         mostrarResultados();
 
+        
         lv.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
                     public void onItemClick(AdapterView<?> parent, View v, int posicion, long id) {
                         buscar((String) lv.getItemAtPosition(posicion));
-                        //String textItem = String.valueOf(parent.getItemAtPosition(posicion));
-                        //Toast.makeText(Agenda.this, textItem, Toast.LENGTH_SHORT).show();
                     }
                 }
         );
-
-        resultados.size();
-        Toast.makeText(this, String.valueOf(resultados.size()) ,Toast.LENGTH_LONG ).show();
     }
 
+    
+    
+    
     private void abrirDBAdapter() {
         try {
+            
+            
             AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "agenda", null, 1);
+            
+            
             SQLiteDatabase bd = admin.getWritableDatabase();
+            
+            
             Cursor fila = bd.rawQuery("select nombre from contactos order by nombre asc", null);
+            
+            
             if (fila.moveToFirst()){
                 do {
                     addToLista(fila);
@@ -71,33 +81,50 @@ public class Agenda extends Activity {
         }
     }
 
+    
+    
     private void addToLista(Cursor fila) {
+        
         resultados.add(fila.getString(0));
     }
 
+    
+    
     private void mostrarResultados() {
+        
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, resultados);
+        
         lv.setAdapter(adapter);
     }
 
+    
+    
     public void agregar(View v){
+        
         Intent i= new Intent(this, Nuevo.class);
         startActivity(i);
         finish();
     }
 
+    
+    
     private void buscar(String dato){
+        
         Intent u= new Intent(this, Detalles.class);
+        
         u.putExtra("nombre", dato);
         startActivity(u);
         finish();
     }
 
+    
+    
     public void busqueda(View v){
-        Intent u= new Intent(this, Busqueda.class);
+        
+        Intent u = new Intent(this, Busqueda.class);
+        
         u.putExtra("nombre", name.getText().toString());
         startActivity(u);
         finish();
     }
-    //el adaftador define la forma en que se van a verse los items
 }
